3288a9a444844a716b0c88c677ae7a55d2332ab4
pa-tiq <patrickdsilva99@gmail.com>
2024-11-29 20:31:57 -0300

    triangle transformations

--- first_app.hpp
+++ first_app.hpp
@@ -10,44 +10,50 @@
 #include "pve_swap_chain.hpp"
 #include "pve_window.hpp"
 
-namespace pve {
-class FirstApp {
-   public:
-    static constexpr int WIDTH = 800;
-    static constexpr int HEIGHT = 600;
+namespace pve
+{
+    class FirstApp
+    {
+    public:
+        static constexpr int WIDTH = 800;
+        static constexpr int HEIGHT = 600;
 
-    FirstApp();
-    ~FirstApp();
+        FirstApp();
+        ~FirstApp();
 
-    FirstApp(const FirstApp &) = delete;
-    FirstApp &operator=(const FirstApp &) = delete;
+        FirstApp(const FirstApp &) = delete;
+        FirstApp &operator=(const FirstApp &) = delete;
 
-    void run();
+        void run();
 
-   private:
-    void sierpinski(
-        std::vector<PveModel::Vertex> &vertices,
-        int depth,
-        glm::vec2 left,
-        glm::vec2 right,
-        glm::vec2 top);
-    void loadGameObjects();
-    void createPipelineLayout();
-    void createPipeline();
-    void createCommandBuffers();
-    void freeCommandBuffers();
-    void drawFrame();
-    void recreateSwapChain();
-    void recordCommandBuffer(int imageIndex);
-    void renderGameObjects(VkCommandBuffer commandBuffer);
+    private:
+        void sierpinski(
+            std::vector<PveModel::Vertex> &vertices,
+            int depth,
+            glm::vec2 left,
+            glm::vec2 right,
+            glm::vec2 top);
+        void sierpinskiTriangle();
+        void spinningTriangle();
+        void crazyTriangles();
 
-    PveWindow pveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
-    PveDevice pveDevice{pveWindow};
-    std::unique_ptr<PveSwapChain> pveSwapChain;
-    // a smart pointer simulates a pointer but with the addition of automatic memory management
-    std::unique_ptr<PvePipeline> pvePipeline;
-    VkPipelineLayout pipelineLayout;
-    std::vector<VkCommandBuffer> commandBuffers;
-    std::vector<PveGameObject> gameObjects;
-};
-}  // namespace pve
+        void loadGameObjects();
+        void createPipelineLayout();
+        void createPipeline();
+        void createCommandBuffers();
+        void freeCommandBuffers();
+        void drawFrame();
+        void recreateSwapChain();
+        void recordCommandBuffer(int imageIndex);
+        void renderGameObjects(VkCommandBuffer commandBuffer);
+
+        PveWindow pveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+        PveDevice pveDevice{pveWindow};
+        std::unique_ptr<PveSwapChain> pveSwapChain;
+        // a smart pointer simulates a pointer but with the addition of automatic memory management
+        std::unique_ptr<PvePipeline> pvePipeline;
+        VkPipelineLayout pipelineLayout;
+        std::vector<VkCommandBuffer> commandBuffers;
+        std::vector<PveGameObject> gameObjects;
+    };
+} // namespace pve
--- pve_game_object.hpp
+++ pve_game_object.hpp
@@ -4,63 +4,77 @@
 
 #include "pve_model.hpp"
 
-namespace pve {
-struct Transform2dComponent {
-    glm::vec2 translation{};  // move objects up, down, left, right
-    glm::mat2 mat2() { return glm::mat2{1.f}; }
-};
-// a Game Object is anything in the game with a collection of properties and methods
-class PveGameObject {
-   public:
-    using id_t = unsigned int;
+namespace pve
+{
+    struct Transform2dComponent
+    {
+        glm::vec2 translation{}; // move objects up, down, left, right
+        glm::vec2 scale{1.f, 1.f};
+        float rotation;
 
-    static PveGameObject createGameObject() {
-        static id_t currentId = 0;
-        return PveGameObject{currentId++};
-    }
+        glm::mat2 mat2()
+        {
+            const float s = glm::sin(rotation);
+            const float c = glm::cos(rotation);
+            glm::mat2 rotMatrix{{c, s}, {-s, c}};
+            glm::mat2 scaleMat({scale.x, .0f}, {.0f, scale.y}); // scale matrix - each {} is a column
+            return rotMatrix * scaleMat;
+        }
+    };
+    // a Game Object is anything in the game with a collection of properties and methods
+    class PveGameObject
+    {
+    public:
+        using id_t = unsigned int;
 
-    // The PveGameObject class uses a unique ID (id_t) to identify objects.
-    // Allowing copies or assignments could result in two objects sharing the same id, breaking the uniqueness constraint.
-    // Deleting these methods ensures that each PveGameObject remains distinct.
+        static PveGameObject createGameObject()
+        {
+            static id_t currentId = 0;
+            return PveGameObject{currentId++};
+        }
 
-    // this deletes the copy constructor.
-    // prevents creating a new PveGameObject by copying an existing one.
-    // Ex: PveGameObject obj1 = PveGameObject::createGameObject();
-    //     PveGameObject obj2 = obj1; // Error: copy constructor is deleted
-    PveGameObject(const PveGameObject &) = delete;
+        // The PveGameObject class uses a unique ID (id_t) to identify objects.
+        // Allowing copies or assignments could result in two objects sharing the same id, breaking the uniqueness constraint.
+        // Deleting these methods ensures that each PveGameObject remains distinct.
 
-    // This deletes the copy assignment operator.
-    // Prevents assigning one PveGameObject to another.
-    // Ex: PveGameObject obj1 = PveGameObject::createGameObject();
-    //     PveGameObject obj2 = PveGameObject::createGameObject();
-    //     obj2 = obj1; // Error: copy assignment operator is deleted
-    PveGameObject &operator=(const PveGameObject &) = delete;
+        // this deletes the copy constructor.
+        // prevents creating a new PveGameObject by copying an existing one.
+        // Ex: PveGameObject obj1 = PveGameObject::createGameObject();
+        //     PveGameObject obj2 = obj1; // Error: copy constructor is deleted
+        PveGameObject(const PveGameObject &) = delete;
 
-    // Moving avoids the cost of copying and ensures the uniqueness of the id is maintained.
-    // The default move constructor and assignment operator are sufficient since the class manages no complex resources like dynamic memory or file handles.
+        // This deletes the copy assignment operator.
+        // Prevents assigning one PveGameObject to another.
+        // Ex: PveGameObject obj1 = PveGameObject::createGameObject();
+        //     PveGameObject obj2 = PveGameObject::createGameObject();
+        //     obj2 = obj1; // Error: copy assignment operator is deleted
+        PveGameObject &operator=(const PveGameObject &) = delete;
 
-    // This uses the default move constructor. Allows moving an object instead of copying it.
-    // Moving transfers ownership of the resource (here, the ID) from the source to the destination without duplication.
-    // Ex: PveGameObject obj1 = PveGameObject::createGameObject();
-    //     PveGameObject obj2 = std::move(obj1); // Move constructor
-    PveGameObject(PveGameObject &&) = default;
+        // Moving avoids the cost of copying and ensures the uniqueness of the id is maintained.
+        // The default move constructor and assignment operator are sufficient since the class manages no complex resources like dynamic memory or file handles.
 
-    // This uses the default move assignment operator. Allows moving an object during assignment.
-    // Similar to the move constructor, it transfers ownership without creating a duplicate.
-    // Ex: PveGameObject obj1 = PveGameObject::createGameObject();
-    //     PveGameObject obj2 = PveGameObject::createGameObject();
-    //     obj2 = std::move(obj1); // Move assignment operator
-    PveGameObject &operator=(PveGameObject &&) = default;
+        // This uses the default move constructor. Allows moving an object instead of copying it.
+        // Moving transfers ownership of the resource (here, the ID) from the source to the destination without duplication.
+        // Ex: PveGameObject obj1 = PveGameObject::createGameObject();
+        //     PveGameObject obj2 = std::move(obj1); // Move constructor
+        PveGameObject(PveGameObject &&) = default;
 
-    const id_t getId() { return id; }
+        // This uses the default move assignment operator. Allows moving an object during assignment.
+        // Similar to the move constructor, it transfers ownership without creating a duplicate.
+        // Ex: PveGameObject obj1 = PveGameObject::createGameObject();
+        //     PveGameObject obj2 = PveGameObject::createGameObject();
+        //     obj2 = std::move(obj1); // Move assignment operator
+        PveGameObject &operator=(PveGameObject &&) = default;
 
-    std::shared_ptr<PveModel> model{};
-    glm::vec3 color{};
-    Transform2dComponent transform2d{};
+        const id_t getId() { return id; }
 
-   private:
-    PveGameObject(id_t objId) : id{objId} {}
+        std::shared_ptr<PveModel> model{};
+        glm::vec3 color{};
+        Transform2dComponent transform2d{};
 
-    id_t id;
-};
-}  // namespace pve
+    private:
+        PveGameObject(id_t objId) : id{objId} {}
+
+        id_t id;
+    };
+} // namespace pve
